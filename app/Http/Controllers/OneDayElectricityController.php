dfb7963f8aed755631243fd2b32377cef29c9ca1
egimaben <egimaben@gmail.com>
2018-12-10 11:03:51 +0300

    refactored OneDayElectricityController to push processing to SQL instead of php

--- app/Http/Controllers/OneDayElectricityController.php
+++ app/Http/Controllers/OneDayElectricityController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Panel;
+use App\Models\OneHourElectricity;
+use Illuminate\Support\Facades\DB;
 
 class OneDayElectricityController extends Controller
 {
@@ -16,6 +18,18 @@ class OneDayElectricityController extends Controller
     public function index(Request $request)
     {
         $panel = Panel::where('serial', $request->panel_serial)->firstOrFail();
+//        $response = OneHourElectricity::select(
+//            array(
+//                DB::Raw('date(hour) day'),
+//                DB::Raw('MIN(kilowatts) min'),
+//                DB::Raw('MAX(kilowatts) max'),
+//                DB::Raw('AVG(kilowatts) average'),
+//                DB::Raw('SUM(kilowatts) sum')))
+//            ->where('panel_id', $panel->id)
+//            ->groupBy('day')
+//            ->get();
+
+
         $hourlies = $panel->oneHourElectricities()->get();
         $dateToHourlyData = $hourlies->pluck('kilowatts', 'hour');
         $dailyData = [];
